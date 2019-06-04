ecc2413fc0bddf8174b36f4627a69bad0b9014ec
Junaid <jahmad095@gmail.com>
2019-06-04 19:58:38 -0400

    finally

--- express_server.js
+++ express_server.js
@@ -7,15 +7,16 @@ const bodyParser = require("body-parser");
 app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({extended: true}));
 
-var urlDatabase = {
+let urlDatabase = {
     'b2xVn2': 'http://www.lighthouse.ca',
     '9sm5xK': 'http://www.google.com'
 }
 
 //***** FUNCTIONS  */
 
-function generateRandomString(longURL){
-
+function generateRandomString(){
+    let short = ( Math.random().toString(36).replace(/[^a-z]+/g, '').substr(0, 5));
+    return short;
 }
 
 
@@ -38,17 +39,30 @@ app.get('/urls', (request, response) =>{
 
 //new url route
 app.post("/urls", (request, response) => {
-    console.log(request.body);
-    response.send("OK");
+    let shortURL = generateRandomString();
+    urlDatabase[shortURL] = request.body.longURL;
+    
+    response.redirect("http://localhost:8000/urls/" + shortURL);
 });
+
 app.get('/urls/new', (request,response)=>{
     response.render("urls_new");
 });
 
 app.get('/urls/:shortURL', (request, response) => {
-    let templateVars = {shortURL: request.params.shortURL, longURL: urlDatabase[request.params.shortURL]};
+    let longURLPass = urlDatabase[request.params.shortURL];
+    let templateVars = {shortURL: request.params.shortURL, longURL: longURLPass};
     response.render('urls_show',templateVars);
+    
+    //response.redirect(longURLPass);
 });
+
+//redirect to actual webstie 
+app.get("/u/:shortURL", (request, response) => {
+    const longURL = urlDatabase[request.params.shortURL];
+    console.log(longURL)
+    response.redirect(longURL);
+  });
 
 app.listen(PORT, () => {
     console.log(`server is listening on port: ${PORT}`);
