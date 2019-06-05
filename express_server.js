88dc78ac179ce80c30a600713c2a1b7fd82d5873
Junaid <jahmad095@gmail.com>
2019-06-05 11:39:50 -0400

    delete added but button doesnt work yet

--- express_server.js
+++ express_server.js
@@ -45,6 +45,7 @@ app.post("/urls", (request, response) => {
     response.redirect(`urls/${shortURL}`);
 });
 
+
 //new url post 
 app.get('/urls/new', (request,response)=>{
     response.render("urls_new");
@@ -62,6 +63,14 @@ app.get("/u/:shortURL", (request, response) => {
     const longURL = urlDatabase[request.params.shortURL];
     console.log(longURL)
     response.redirect(longURL);
+
+});
+
+app.post(`/urls/:shortURL/delete`,(request, response) => {
+    const shortURL = request.params.shortURL;
+    delete urlDatabase[shortURL];
+    console.log(urlDatabase)
+    response.redirect("/urls");
 });
 
 app.listen(PORT, () => {
